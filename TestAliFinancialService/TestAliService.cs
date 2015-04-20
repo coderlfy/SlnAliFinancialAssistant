5e202c8e1844e3a993641cd88454c9e2cc53c639
coderlfy <13403692778@126.com>
2015-04-20 22:18:26 +0800

    单元测试

---
+++ TestAliFinancialService/TestAliService.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestAliFinancialService
+{
+    
+    class TestAliService
+    {
+        [Test]
+        public void TestGetCurrentRate()
+        {
+            AliService.AliClient aliclient = new AliService.AliClient();
+            Console.WriteLine(aliclient.GetJsonCurrentRate());
+
+        }
+    }
+}
