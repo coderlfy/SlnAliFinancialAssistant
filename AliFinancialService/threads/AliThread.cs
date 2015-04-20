ef1a252b03c9405b867c1313d15fbd93652af24f
coderlfy <13403692778@126.com>
2015-04-20 23:37:25 +0800

    爬取线程

--- AliFinancialService/threads/AliThread.cs
+++ AliFinancialService/threads/AliThread.cs
@@ -19,11 +19,16 @@ namespace AliFinancialService.threads
             {
                 //爬取ali利率？？
                 //若成功爬取，则走以下代码？？
+                //if(成功条件)
+                //{ 
+                    if (AliCache._CurrentRate == null)
+                        AliCache._CurrentRate = new Ali.Model.CurrentRate();
 
-                if (AliCache._CurrentRate == null)
-                    AliCache._CurrentRate = new Ali.Model.CurrentRate();
-
-
+                    AliCache._CurrentRate._FetchTime = DateTime.Now;
+                    AliCache._CurrentRate._Month1To6 = 4.57M;
+                    AliCache._CurrentRate._Month6To12 = 5.55M;
+                    AliCache._CurrentRate._Month6To12 = 6.32M;
+                //}
                 
             }, secondsinterval);
         }
