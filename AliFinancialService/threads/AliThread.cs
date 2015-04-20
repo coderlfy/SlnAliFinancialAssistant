d4ddb8d2ccf1cb78989798d57a454a764bd24d69
coderlfy <13403692778@126.com>
2015-04-20 22:22:23 +0800

    lihuajun欲完成部分

--- AliFinancialService/threads/AliThread.cs
+++ AliFinancialService/threads/AliThread.cs
@@ -17,8 +17,8 @@ namespace AliFinancialService.threads
             int secondsinterval = 1;
             base.StartWhile(() =>
             {
-                //爬取ali利率
-                //若成功爬取，则走以下代码
+                //爬取ali利率？？
+                //若成功爬取，则走以下代码？？
 
                 if (AliCache._CurrentRate == null)
                     AliCache._CurrentRate = new Ali.Model.CurrentRate();
