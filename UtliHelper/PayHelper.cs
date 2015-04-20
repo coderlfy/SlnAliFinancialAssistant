428c2a9eaa30b34db0484ad53b3deb73ed38dbe4
QQ81867376 <81867376@QQ.com>
2015-04-20 23:49:07 +0800

    获取利率

--- UtliHelper/PayHelper.cs
+++ UtliHelper/PayHelper.cs
@@ -1,11 +1,11 @@
-﻿using HtmlAgilityPack;
-using MoneyCalculator.Helpers;
+﻿using HtmlAgilityPack; 
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using UtliHelper;
 
 namespace MoneyCalculator
 {
