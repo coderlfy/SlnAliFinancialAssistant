beb96457d66d89bde647f642d559194d1fd4b1ce
QQ81867376 <81867376@QQ.com>
2015-04-23 23:39:43 +0800

    123

--- AliFinancialService/threads/AliThread.cs
+++ AliFinancialService/threads/AliThread.cs
@@ -9,27 +9,30 @@ namespace AliFinancialService.threads
     {
         public AliThread(bool enableStop)
             : base(enableStop)
-        { 
-        
+        {
+
         }
         public override void Start()
         {
-            int secondsinterval = 1;
+            int secondsinterval = 60;
             base.StartWhile(() =>
             {
                 //爬取ali利率？？
                 //若成功爬取，则走以下代码？？
                 //if(成功条件)
                 //{ 
+                List<decimal> data = UtliHelper.PayHelper.GetPayInfo();
+                if (data != null)
+                {
                     if (AliCache._CurrentRate == null)
                         AliCache._CurrentRate = new Ali.Model.CurrentRate();
-
                     AliCache._CurrentRate._FetchTime = DateTime.Now;
-                    AliCache._CurrentRate._Month1To6 = 4.57M;
-                    AliCache._CurrentRate._Month6To12 = 5.55M;
-                    AliCache._CurrentRate._Month12To24 = 6.32M;
+                    AliCache._CurrentRate._Month1To6 = data[1];
+                    AliCache._CurrentRate._Month6To12 = data[2];
+                    AliCache._CurrentRate._Month12To24 = data[3];
+                }
                 //}
-                
+
             }, secondsinterval);
         }
     }
